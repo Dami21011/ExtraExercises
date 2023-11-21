f4e7215170aff27684f164d5d699fe412b8f6f64
Damian <damrodlop@alu.edu.gva.es>
2023-11-21 15:10:51 +0100

    factorial added

---
+++ src/factorial/Factorial.java
@@ -0,0 +1,32 @@
+package factorial;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        // Utilizar un Scanner para obtener la entrada del usuario
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicitar al usuario que ingrese un número
+        System.out.print("Introduce un número: ");
+        int numero = scanner.nextInt();
+
+        // Calcular el factorial del número
+        long factorial = calcularFactorial(numero);
+
+        // Imprimir el resultado
+        System.out.println("El factorial de " + numero + " es: " + factorial);
+
+        // Cerrar el Scanner
+        scanner.close();
+    }
+
+    // Método para calcular el factorial de un número
+    private static long calcularFactorial(int numero) {
+        if (numero == 0) {
+            return 1;
+        } else {
+            return numero * calcularFactorial(numero - 1);
+        }
+    }
+}
